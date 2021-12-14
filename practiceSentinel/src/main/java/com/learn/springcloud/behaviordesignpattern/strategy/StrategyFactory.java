d61d7dd0a816d31b2cf6a8ed1560181feaf2a62a
David Shao <597701971@qq.com>
2021-12-14 14:26:58 +0800

    策略模式

---
+++ practiceSentinel/src/main/java/com/learn/springcloud/behaviordesignpattern/strategy/StrategyFactory.java
@@ -0,0 +1,25 @@
+package com.learn.springcloud.behaviordesignpattern.strategy;
+
+import java.util.HashMap;
+
+/**
+ * @author sdw
+ * @version 1.0
+ * @className StrategyFactory
+ * @date 2021/12/14 14:03:40
+ * @description
+ */
+
+public class StrategyFactory {
+    private HashMap<String,Strategy> strategyHashMap;
+    {
+        strategyHashMap = new HashMap<>();
+        strategyHashMap.put("first",new FirstStrategy());
+        strategyHashMap.put("second",new SecondStrategy());
+    }
+
+
+    public Strategy getStrategy(String type){
+        return strategyHashMap.get(type);
+    }
+}
