1a6ee8420a735b6c7bfa0afa33fcd875e5e21d93
David Shao <597701971@qq.com>
2021-12-21 18:05:24 +0800

    实现一个限流器

--- practiceSentinel/src/main/java/com/learn/springcloud/test/limit/Limiter.java
+++ practiceSentinel/src/main/java/com/learn/springcloud/test/limit/Limiter.java
@@ -4,6 +4,11 @@ package com.learn.springcloud.test.limit;
  * @author sdw
  * @date 2021/12/21 17:43:25
  * @description
+ *              框架考虑：通用、性能、容错、易拓展
+ *              限流规则配置：最小惊奇原则，要支持 .property  .yaml  .xml
+ *              限流算法：时间窗口、滑动时间窗口、令牌桶、
+ *              限流模式：单应用限流、分布式限流
+ *              使用: 代码入侵要低
  */
 
 public interface Limiter {
