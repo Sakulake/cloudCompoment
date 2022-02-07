3271ef41c70e9f6c20ef5d3441b269826c27b73f
shaodawei <597701971@qq.com>
2022-02-07 15:57:07 +0800

    图，深度优先（递归，通过线程栈） 广度优先（使用队列记录需要访问的顶）

--- practiceSentinel/src/main/java/com/learn/springcloud/datastructuresandalgorithms/leecode/Solution2029.java
+++ practiceSentinel/src/main/java/com/learn/springcloud/datastructuresandalgorithms/leecode/Solution2029.java
@@ -1,81 +1,71 @@
 package com.learn.springcloud.datastructuresandalgorithms.leecode;
 
 public class Solution2029 {
+
+    int mod = 1000000007;
     /**
-     * Alice 和 Bob 再次设计了一款新的石子游戏。现有一行 n 个石子，每个石子都有一个关联的数字表示它的价值。给你一个整数数组 stones ，其中 stones[i] 是第 i 个石子的价值。
+     * 给你一个整数n，请你帮忙统计一下我们可以按下述规则形成多少个长度为n的字符串：
      *
-     * Alice 和 Bob 轮流进行自己的回合，Alice 先手。每一回合，玩家需要从 stones中移除任一石子。
-     *
-     * 如果玩家移除石子后，导致 所有已移除石子 的价值总和 可以被 3 整除，那么该玩家就 输掉游戏 。
-     * 如果不满足上一条，且移除后没有任何剩余的石子，那么 Bob 将会直接获胜（即便是在 Alice 的回合）。
-     * 假设两位玩家均采用最佳 决策。如果 Alice 获胜，返回 true ；如果 Bob 获胜，返回 false 。
-     *
-     * 将 数组内数据分为三类
-     * 第一类  除3余1
-     * 第二类  除3余2
-     * 第三类  除3余0
-     *
-     * 如果 第三类数 的数量为偶数，且其他类数量不为0，这不需要考虑 三类数的操作， 相当于换手
-     *
-     * Alice 先手1     1 1 2 1 2 1 2 1 2 1 2 ... 抛出2个一类数后，  如果 一类数量 = 二类数量  Alice输    一类数量>二类数量 Alice 输  一类数量<二类数量  Alice赢
-     *
-     * Alice 先手2     2 2 1 2 1 2 1 2 1 2 1 ...  抛出2个二类数后， 如果 一类数量 = 二类数量  Alice输    一类数量>二类数量 Alice 赢  一类数量<二类数量  Alice输
+     * 字符串中的每个字符都应当是小写元音字母（'a', 'e', 'i', 'o', 'u'）
+     * 每个元音'a'后面都只能跟着'e'
+     * 每个元音'e'后面只能跟着'a'或者是'i'
+     * 每个元音'i'后面不能 再跟着另一个'i'
+     * 每个元音'o'后面只能跟着'i'或者是'u'
+     * 每个元音'u'后面只能跟着'a'
+     * 由于答案可能会很大，所以请你返回 模10^9 + 7之后的结果。
      *
      *
      *
      *
-     * 如果 除 第三类数 其他类数量且为0 ，Alise 输
+     * a前头可以是 e i u
+     * e前头可以是 a i
+     * i前头可以是 e o
+     * o前头可以是 i
+     * u前头可以是 i o
      *
      *
      *
-     * 如果 第三类数 的数量为奇数，且其他类数量且不为0
      *
-     * Alice 先手1  1 3 1
-     *                      2 1 2 1 2 1 2 1 2    抛出2个1类数据后   如果 一类数量 = 二类数量  Alice赢   一类数量>二类数量 Alice 赢  一类数量<二类数量  Alice输
+     * 使用二维数组 int array [5][n] 记录次数
+     * 一层数组 代表以某元音字母结尾
+     * 0 代表 a
+     * 1 代表 e
+     * 2 代表 i
+     * 3 代表 o
+     * 4 代表 u
      *
-     *  Alice 先手2  2 3 2
-     *                      1 1 2 1 2 1 2 1 2 1 2
+     * 二层数组
+     * 代表下标长度的字符串总数
      *
+     * array[0][n] = array[1][n-1] + array[2][n-1] + array[4][n-1]
+     * array[1][n] = array[0][n-1] + array[2][n-1]
+     * array[2][n] = array[1][n-1] + array[3][n-1]
+     * array[3][n] = array[2][n-1]
+     * array[4][n] = array[2][n-1] + array[3][n-1]
+     *
+     * 5个变量相加，就是最终得数
      *
      */
-    public boolean stoneGameIX(int[] stones) {
-        if(stones.length == 0){
-            return false;
+    public int countVowelPermutation(int n) {
+        int array [][] = new int[5][n+1];
+        array[0][1] =1;
+        array[1][1] =1;
+        array[2][1] =1;
+        array[3][1] =1;
+        array[4][1] =1;
+        for (int i=2;i<n+1;i++){
+            array[0][i] =  (array[1][i-1] +array[2][i-1] +array[4][i-1])%mod;
+            array[1][i] =  (array[0][i-1] +array[2][i-1])%mod;
+            array[2][i] = ( array[1][i-1] +array[3][i-1])%mod;
+            array[3][i] =  (array[2][i-1])%mod;
+            array[4][i] = (array[2][i-1] + array[3][i-1])%mod;
         }
 
-        int firstCount = 0;
-        int secondCount = 0;
-        int thirdCount = 0;
-
-        for(int i =0;i<stones.length;i++){
-            int j = stones[i]%3;
-            if(j==0){
-                thirdCount++;
-            }else if (j==1){
-                firstCount++;
-            }else {
-                secondCount++;
-            }
-        }
-
-        if(firstCount ==0 && secondCount==0 ){
-            return false;
-        }
-
-        if(thirdCount%2 ==0){
-            if(firstCount-2<secondCount  || secondCount -2 < firstCount){
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            if(firstCount-2<secondCount  || secondCount -2< firstCount ){
-                return false;
-            }else{
-                return true;
-            }
-        }
+        return  (array[0][n]+array[1][n]+array[2][n]+array[3][n]+array[4][n]) %mod ;
 
     }
 
+    public static void main(String[] args) {
+        System.out.println(new Solution1220().countVowelPermutation(5));
+    }
 }
