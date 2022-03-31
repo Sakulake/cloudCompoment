797f303c67951d67a4c0adf848420acad4512d1e
shaodawei <597701971@qq.com>
2022-03-31 21:18:04 +0800

    test rocketmq

---
+++ practiceSentinel/src/main/java/com/learn/springcloud/mq/RocketMQConsumer.java
@@ -0,0 +1,61 @@
+package com.learn.springcloud.mq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListener;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+public class RocketMQConsumer {
+    /**
+     * 生产者的组名
+     */
+    @Value("${ali.rocketmq.consumerGroup}")
+    private String consumerGroup;
+
+    /**
+     * NameServer 地址
+     */
+    @Value("${ali.rocketmq.namesrvaddr}")
+    private String namesrvAddr;
+
+    @PostConstruct
+    public void consume(){
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(consumerGroup);
+        consumer.setNamesrvAddr(namesrvAddr);
+        try {
+            consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+            consumer.subscribe("user-topic","*");
+            consumer.registerMessageListener(new MessageListenerConcurrently() {
+                @Override
+                public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                    try {
+                        for (MessageExt messageExt : list) {
+
+                            System.err.println("消费消息: " + new String(messageExt.getBody()));//输出消息内容
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return ConsumeConcurrentlyStatus.RECONSUME_LATER; //稍后再试
+                    }
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS; //消费成功
+                }
+            });
+
+            consumer.start();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ practiceSentinel/src/main/java/com/learn/springcloud/mq/RocketMQProducer.java
@@ -0,0 +1,63 @@
+package com.learn.springcloud.mq;
+
+import com.alibaba.fastjson.JSON;
+import com.learn.springcloud.bo.RokectMQMessage;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.UnsupportedEncodingException;
+
+@Component
+public class RocketMQProducer {
+    /**
+     * 生产者的组名
+     */
+    @Value("${ali.rocketmq.producerGroup}")
+    private String producerGroup;
+
+    /**
+     * NameServer 地址
+     */
+    @Value("${ali.rocketmq.namesrvaddr}")
+    private String namesrvAddr;
+
+
+    @PostConstruct
+    public void producer(){
+        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
+        producer.setNamesrvAddr(namesrvAddr);
+        try {
+            producer.start();
+            for (int i = 0; i < 100; i++) {
+                RokectMQMessage userContent = new RokectMQMessage(String.valueOf(i),"abc"+i);
+                String jsonstr = JSON.toJSONString(userContent);
+                System.out.println("发送消息:"+jsonstr);
+                Message message = new Message("user-topic", "user-tag", jsonstr.getBytes(RemotingHelper.DEFAULT_CHARSET));
+                SendResult result = producer.send(message);
+                System.err.println("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
+            }
+
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        } catch (MQBrokerException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            producer.shutdown();
+        }
+    }
+
+}
